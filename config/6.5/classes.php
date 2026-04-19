e75797ff7b17b21a021f7de00a10dd354ca8316a
Kama <tkama@bk.ru>
2026-04-19 21:35:01 +0500

    Config for 6.5 added

---
+++ config/6.5/classes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Configuration for the WP Copy parser.
+ * Filenames and functions to be copied from the WordPress core.
+ */
+
+return [
+	'wp-includes/class-wp-exception.php' => false, // WP_Exception
+	'wp-includes/html-api/class-wp-html-doctype-info.php' => false, // WP_HTML_Doctype_Info
+	'wp-includes/html-api/class-wp-html-decoder.php' => false, // WP_HTML_Decoder
+];
---
+++ config/6.5/functions/wp-includes/load.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+	'absint' => false, // del from main confit & move to enother file
+];
