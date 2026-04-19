c2af66e5d6f9fda69e815b6d2f8f3c842564870b
Kama <tkama@bk.ru>
2026-04-19 19:52:26 +0500

    Initial branch creation

--- wp-runtime/copy/classes/WP_Exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// ------------------auto-generated---------------------
-
-// wp-includes/class-wp-exception.php (WP 6.7.5)
-if( ! class_exists( 'WP_Exception' ) ) :
-	class WP_Exception extends Exception {}
-endif;
-
