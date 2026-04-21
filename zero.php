6dc01694d44539978ea496f85105c0cabf160a51
Kama <tkama@bk.ru>
2026-04-22 00:53:46 +0500

    Release 6.8.1.2

--- zero.php
+++ zero.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * This file allow you to load the wp-runtime directly without composer autoloader.
+ * Can be useful if you use this lib as submodule or copy-paste into your project,
+ * and don't want to setup composer autoloading.
+ */
 
 require_once __DIR__ . '/wp-runtime/Bootstrap.php';
 \Unitest_WP_Copy\Bootstrap::init();
