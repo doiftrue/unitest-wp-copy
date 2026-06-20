8b81049f1f0ae2c00c1c94858979b9dbeaee5835
Kama <tkama@bk.ru>
2026-06-21 02:54:24 +0500

    Release 6.9.2.1

--- wp-runtime/boot-wp-constants.php
+++ wp-runtime/boot-wp-constants.php
@@ -5,3 +5,11 @@ defined( 'WPINC' )               || define( 'WPINC', 'wp-includes' );
 defined( 'WP_CONTENT_DIR' )      || define( 'WP_CONTENT_DIR', '/path/to/wp/wp-content' );
 defined( 'WP_CONTENT_URL' )      || define( 'WP_CONTENT_URL', 'https://test.dev/wp-content' );
 defined( 'WP_ENVIRONMENT_TYPE' ) || define( 'WP_ENVIRONMENT_TYPE', 'local' );
+
+/// from class-wpdb.php
+defined( 'EZSQL_VERSION' ) || define( 'EZSQL_VERSION', 'WP1.25' );
+defined( 'OBJECT' )        || define( 'OBJECT', 'OBJECT' );
+defined( 'object' )        || define( 'object', 'OBJECT' );
+defined( 'OBJECT_K' )      || define( 'OBJECT_K', 'OBJECT_K' );
+defined( 'ARRAY_A' )       || define( 'ARRAY_A', 'ARRAY_A' );
+defined( 'ARRAY_N' )       || define( 'ARRAY_N', 'ARRAY_N' );
