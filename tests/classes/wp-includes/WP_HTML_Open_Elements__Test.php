1900f15331ba2ba78fa355a1c5b8f8d784a612a1
Timur_Kamaev <Timur_Kamaev@epam.com>
2026-07-30 21:40:46 +0500

    7.0.3.2 - Adaptation

--- tests/classes/wp-includes/WP_HTML_Open_Elements__Test.php
+++ tests/classes/wp-includes/WP_HTML_Open_Elements__Test.php
@@ -11,11 +11,17 @@ class WP_HTML_Open_Elements__Test extends \PHPUnit\Framework\TestCase {
 		$this->assertTrue( $elements->contains_node( $token ) );
 		$this->assertSame( $token, $elements->current_node() );
 		$this->assertSame( 1, $elements->count() );
-		$this->assertTrue( $elements->pop() );
-		$this->assertSame( 0, $elements->count() );
 
 		if( wp_version_compare( '>= 6.7.0' ) ){
 			$this->assertTrue( $elements->contains( 'DIV' ) );
+		}
+
+		$this->assertTrue( $elements->pop() );
+		$this->assertSame( 0, $elements->count() );
+		$this->assertFalse( $elements->contains_node( $token ) );
+
+		if( wp_version_compare( '>= 6.7.0' ) ){
+			$this->assertFalse( $elements->contains( 'DIV' ) );
 		}
 	}
 }
